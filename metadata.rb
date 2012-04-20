9b437c034503b68b018bf077b99815443eb25122
Brad Montgomery <brad@bradmontgomery.net>
2012-04-20 11:52:27 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "bmontgomery@coroutine.com"
 license          "Apache 2.0"
 description      "Installs/Configures elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 depends "java"
